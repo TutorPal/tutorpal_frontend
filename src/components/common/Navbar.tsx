ad672819435906d83a2d0334f1ddf683aaffeaca
Emase <corneliuslochipi@gmail.com>
2024-11-15 12:50:28 +0300

    updated navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link";
+import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 import ConnectWalletButton from "./ConnectWalletButton";
 
 const Navbar = () => {
@@ -35,57 +36,51 @@ const Navbar = () => {
         </div>
 
         <div className="hidden md:block">
-          <Link href="#get-started">
-            <ConnectWalletButton />
-          </Link>
+          <ConnectWalletButton />
         </div>
 
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="focus:outline-none">
-            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-          </button>
+          <Popover open={isOpen} onOpenChange={toggleMenu}>
+            <PopoverTrigger>
+              <span className="focus:outline-none">
+                {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+              </span>
+            </PopoverTrigger>
+
+            <PopoverContent className="bg-gray-800 text-gray-300 w-full text-center">
+              <Link
+                href="#courses"
+                onClick={() => setIsOpen(false)}
+                className="block px-6 py-3 hover:bg-gray-700"
+              >
+                Courses
+              </Link>
+              <Link
+                href="#consultations"
+                onClick={() => setIsOpen(false)}
+                className="block px-6 py-3 hover:bg-gray-700"
+              >
+                Consultations
+              </Link>
+              <Link
+                href="#about"
+                onClick={() => setIsOpen(false)}
+                className="block px-6 py-3 hover:bg-gray-700"
+              >
+                About Us
+              </Link>
+              <Link
+                href="#support"
+                onClick={() => setIsOpen(false)}
+                className="block px-6 py-3 hover:bg-gray-700"
+              >
+                Support
+              </Link>
+              <ConnectWalletButton />
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
-
-      {isOpen && (
-        <div className="md:hidden bg-gray-800 text-gray-300">
-          <Link
-            href="#courses"
-            onClick={toggleMenu}
-            className="block px-6 py-3 hover:bg-gray-700"
-          >
-            Courses
-          </Link>
-          <Link
-            href="#consultations"
-            onClick={toggleMenu}
-            className="block px-6 py-3 hover:bg-gray-700"
-          >
-            Consultations
-          </Link>
-          <Link
-            href="#about"
-            onClick={toggleMenu}
-            className="block px-6 py-3 hover:bg-gray-700"
-          >
-            About Us
-          </Link>
-          <Link
-            href="#support"
-            onClick={toggleMenu}
-            className="block px-6 py-3 hover:bg-gray-700"
-          >
-            Support
-          </Link>
-          <Link
-            href="#get-started"
-            onClick={toggleMenu}
-            className="block px-6 py-3 text-center bg-indigo-600 hover:bg-indigo-500"
-          >
-            Get Started
-          </Link>
-        </div>
-      )}
     </nav>
   );
 };
