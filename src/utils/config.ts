4ba74d1cfea744d0edd4d83ea11badceac3dbd72
Proper <properprogress4@gmail.com>
2024-11-21 21:00:44 +0100

    pinata config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,7 @@
 "server only";
 
 import { PinataSDK } from "pinata-web3";
-import "dotenv/config";
-
-
 export const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT,
-  pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL,
+  pinataJwt: `${process.env.PINATA_JWT}`,
+  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
 });
-
