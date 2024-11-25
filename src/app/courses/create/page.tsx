a0b019ecd449b29bda91768db56e27d75815f7d2
Levi Francis <levifrancis15@gmail.com>
2024-11-25 18:35:28 +0100

    add course display and create functionality

--- src/app/courses/create/page.tsx
+++ src/app/courses/create/page.tsx
@@ -4,8 +4,18 @@ import { Navigation } from '@/components/navigation'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { useToast } from "@/hooks/use-toast";
+import Navbar from '@/components/common/Navbar'
+import axios from "axios";
+import LoadingSpinner from '@/components/common/LoadingSpinner'
+import { useWriteContract } from 'wagmi'
+import { tutorPalMarketAddress } from '@/utils/constants'
+import { tutorPalAbi } from '@/abi/tutorPalAbi'
+
+interface Metadata {
+  title: string;
+}
 
 export default function CreateCourse() {
   const [formData, setFormData] = useState({
@@ -16,20 +26,66 @@ export default function CreateCourse() {
     price: '',
     royalty: '',
   })
+  const [file, setFile] = useState<File | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [metadata] = useState<Metadata>({
+      title: "",
+  });
+
+  const [responseUrls, setResponseUrls] = useState<{
+    fileUrl: string;
+    metadataUrl: string;
+} | null>(null);
+
   const { toast } = useToast()
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+        setFile(e.target.files[0]);
+    }
+};
+
+// Write contract hook for user registration
+const { writeContract, isPending, isSuccess } = useWriteContract();
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     // Here you would typically interact with your smart contract
+    // string memory _title,
+    //     string memory _symbol,
+    //     string memory _metadataURI,
+    //     uint256 _maxSupply,
+    //     uint256 _price,
+    //     uint16 _royalty
+
+    writeContract({
+      address: tutorPalMarketAddress,
+      abi: tutorPalAbi,
+      functionName: 'createCourse',
+      args: [
+        formData.title, 
+        formData.symbol, 
+        responseUrls?.metadataUrl, 
+        formData.maxSupply, 
+        formData.price,
+        formData.royalty
+      ]
+    });
+
     console.log('Form submitted:', formData)
-    toast({
-      title: "Course Created",
-      description: "Your course has been successfully created.",
-    })
+    if(isSuccess) {
+      toast({
+        title: "Course Created",
+        description: "Your course has been successfully created.",
+      })
+    }
     // Reset form after submission
     setFormData({
       title: '',
@@ -41,11 +97,72 @@ export default function CreateCourse() {
     })
   }
 
+  const handleFileUpload = async (e: FormEvent) => {
+    e.preventDefault();
+    setErrorMessage("");
+    setIsLoading(true)
+
+    if (!file) {
+        setErrorMessage("Please upload a file.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const pinataMetadata = {
+        name: `${metadata.title}`,
+        keyvalues: metadata,
+    };
+    formData.append("pinataMetadata", JSON.stringify(pinataMetadata));
+
+    try {
+        const response = await axios.post("/api/ipfs-uploads", formData, {
+            headers: { 
+                "Content-Type": "multipart/form-data"
+            },
+        });
+
+        setResponseUrls({
+            fileUrl: response.data.fileUrl,
+            metadataUrl: response.data.metadataUrl || "N/A"
+        });
+
+        toast({
+          title: "Upload Successful",
+          description: "Your course file uploaded successfully.",
+        })
+        
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            console.error("Axios error:", error.response?.data || error.message);
+            setErrorMessage("Failed to upload file. Please try again.");
+        } else if (error instanceof Error) {
+            console.error("Error uploading file:", error.message);
+            setErrorMessage("An unexpected error occurred. Please try again.");
+        } else {
+            console.error("Unknown error:", error);
+            setErrorMessage("An unknown error occurred. Please contact support.");
+        }
+
+        toast({
+          title: "Upload Failed",
+          description: errorMessage
+        })
+    } finally {
+        setIsLoading(false);
+    }
+};
+
   return (
+    <>
+    <Navbar />
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">Create a New Course</h1>
-      <Navigation />
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+      {/* <Navigation /> */}
+      <form 
+      // onSubmit={handleSubmit} 
+      className="max-w-md mx-auto">
         <div className="mb-4">
           <Label htmlFor="title">Course Title</Label>
           <Input
@@ -54,6 +171,7 @@ export default function CreateCourse() {
             name="title"
             value={formData.title}
             onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
@@ -65,17 +183,37 @@ export default function CreateCourse() {
             name="symbol"
             value={formData.symbol}
             onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
+
+        <div className="mb-4 w-full">
+                    <label className="block mb-2 text-black">Course File</label>
+                    <div className='flex w-full'>
+                      <div>
+
+                    <Input
+                        type="file"
+                        onChange={handleFileChange}
+                        className="w-full p-2 border rounded text-black"
+                    />
+                      </div>
+                      <div>
+                        <button onClick={handleFileUpload} disabled={isLoading} className='rounded p-2 text-sm border'>{isLoading ? <LoadingSpinner /> : "proceed"}</button>
+                      </div>
+                    </div>
+                </div>
+
         <div className="mb-4">
           <Label htmlFor="metadataURI">Metadata URI</Label>
           <Input
             type="text"
             id="metadataURI"
             name="metadataURI"
-            value={formData.metadataURI}
-            onChange={handleChange}
+            value={responseUrls?.metadataUrl}
+            // onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
@@ -87,6 +225,7 @@ export default function CreateCourse() {
             name="maxSupply"
             value={formData.maxSupply}
             onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
@@ -98,6 +237,7 @@ export default function CreateCourse() {
             name="price"
             value={formData.price}
             onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
@@ -109,12 +249,14 @@ export default function CreateCourse() {
             name="royalty"
             value={formData.royalty}
             onChange={handleChange}
+            className='rounded'
             required
           />
         </div>
-        <Button type="submit" className="w-full">Create Course</Button>
+        <Button onClick={handleSubmit} type="submit" disabled={isPending} className="w-full">{isPending ? <LoadingSpinner /> : "Create Course"}</Button>
       </form>
     </div>
+    </>
   )
 }
 
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -7,6 +7,8 @@ import Navbar from '@/components/common/Navbar'
 import { useReadContract } from 'wagmi'
 import { tutorPalAbi } from '@/abi/tutorPalAbi'
 import { tutorPalMarketAddress } from '@/utils/constants'
+// import { useState } from 'react'
+import { useCourses } from '@/hooks/useCourses'
 import { useState } from 'react'
 
 // This would typically come from an API call to your smart contract
@@ -17,7 +19,8 @@ const mockCourses = [
 
 export default function Courses() {
 
-    const [courses] = useState<string[]>([]);
+    // const [courses, setCourses] = useState<string[]>([]);
+    // const { courses, isLoading } = useCourses(tutorPalAbi, tutorPalMarketAddress);
 
     // useWatchContractEvent({
     //     address: tutorPalMarketAddress,
@@ -29,11 +32,38 @@ export default function Courses() {
     //     },
     // })
 
-    const { data, isLoading } = useReadContract({
-        abi: tutorPalAbi,
-        address: tutorPalMarketAddress,
-        functionName: 'getAllCourses',
-    })
+    // const { data, isLoading } = useReadContract({
+    //     abi: tutorPalAbi,
+    //     address: tutorPalMarketAddress,
+    //     functionName: 'getAllCourses',
+    // })
+
+    const { data:courseId, isLoading:isCourseIdLoading } = useReadContract({
+      abi: tutorPalAbi,
+      address: tutorPalMarketAddress,
+      functionName: 'createCourseCount',
+  })
+
+  const { data: course, isLoading:isCourseLoading } = useReadContract({
+    abi: tutorPalAbi,
+    address: tutorPalMarketAddress,
+    functionName: 'getCoursebyId',
+    args: [Number(0)]
+})
+console.log("COURSE", course)
+
+// Function to convert BigInt values to regular numbers/strings
+// const serializeCourseData = (course:any) => {
+//   const serializedCourse = {} as any;
+//   for (const [key, value] of Object.entries(course)) {
+//     if (typeof value === 'bigint') {
+//       serializedCourse[key] = value.toString();
+//     } else {
+//       serializedCourse[key] = value;
+//     }
+//   }
+//   return serializedCourse;
+// };
 
   return (
     <>
@@ -42,23 +72,28 @@ export default function Courses() {
         <h1 className="text-4xl font-bold mb-8">Available Courses</h1>
         {/* <Navigation /> */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {JSON.stringify(courses)}
-            {isLoading && <div>Loading..</div>}
-            {JSON.stringify(data)}
-            {mockCourses.map((course, i) => (
-                <CourseCard key={i} course={course} />
-            // <div key={course.id} className="bg-white p-6 rounded-lg shadow-md">
-            //     <h2 className="text-2xl font-semibold mb-2">{course.title}</h2>
-            //     <p className="mb-2">Instructor: {course.instructor}</p>
-            //     <p className="mb-4">Price: {course.price}</p>
-            //     <Link href={`/courses/${course.id}`}>
-            //     <Button>View Details</Button>
-            //     </Link>
-            // </div>
-            ))}
+            {/* {JSON.stringify(courses)} */}
+            {/* {isLoading && <div>Loading..</div>} */}
+            {isCourseIdLoading && <div>id loading..</div>}
+            {/* {JSON.stringify(Number(courseId))} */}
+            {/* {isCourseLoading && <div>Course Loading</div>} */}
+            {/* {JSON.stringify(courses)} */}
+            {/* {mockCourses.map((course, i) => ( */}
+            {isCourseLoading ? <div>Loading...</div> :
+                <CourseCard course={course} />
+            }
+              {/* // ))} */}
         </div>
         </div>
     </>
   )
 }
+// <div key={course.id} className="bg-white p-6 rounded-lg shadow-md">
+//     <h2 className="text-2xl font-semibold mb-2">{course.title}</h2>
+//     <p className="mb-2">Instructor: {course.instructor}</p>
+//     <p className="mb-4">Price: {course.price}</p>
+//     <Link href={`/courses/${course.id}`}>
+//     <Button>View Details</Button>
+//     </Link>
+// </div>
 
